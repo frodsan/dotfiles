5fafacc5c40b1663aa0d6a7c6564e61c6519e2ac
Francesco Rodriguez <frodsan@protonmail.com>
2017-12-05 22:15:31 +0100

    Add vscode config.

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 BLACKLIST=".
 ..
 .git
+vscode
 "
 
 for file in .*; do
@@ -10,4 +11,23 @@ for file in .*; do
     rm -rf $HOME/$file
     ln -s $PWD/$file $HOME/$file
   fi
+done
+
+# Setting VS Code
+
+if [[ ! -d "$HOME/Library/Application Support/Code" ]]; then
+  mkdir -p "$HOME/Library/Application Support/Code"
+fi
+
+if [[ ! -d "$HOME/Library/Application Support/Code/User" ]]; then
+  mkdir -p "$HOME/Library/Application Support/Code/User"
+fi
+
+ln -s "$PWD/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+
+VSCODE_EXTENSIONS=(
+)
+
+for EXTENSION in ${VSCODE_EXTENSIONS[@]}; do
+  code --install-extension $EXTENSION
 done
